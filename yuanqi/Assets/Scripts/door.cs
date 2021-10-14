17430340545fa8054d8cf1ddbed086fff9516b35
67555 <2276708020@qq.com>
2021-10-14 20:56:31 +0800

    revise

---
+++ yuanqi/Assets/Scripts/door.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tilemap = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
